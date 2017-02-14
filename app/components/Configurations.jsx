0e6c6a44c5eacb9b877967a8576e86a98be58548
Nils Larsgård <nilsmagnus@gmail.com>
2017-02-14 22:06:10 +0100

    Start some actions, need redux soon

--- app/components/Configurations.jsx
+++ app/components/Configurations.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, {PropTypes, Component} from 'react';
 
-const Configurations = ({ greet, message }) => (
-    <p>
-        Hide input
-        <input type="checkbox"/>
-        Text size
-        <input type="number" maxLength="3" defaultValue="100"/>
-        Columns
-        <select>
-            <option value="1">1</option>
-            <option value="2">2</option>
-        </select>
 
-    </p>
-);
+const Configurations = ({inputHidden, toggleShowInput, toggleBoxes}) =>
+    (
+        <p>
+            <span >{inputHidden ? "" : "Hide input"}</span>
+            <input type="checkbox" onClick={() => toggleShowInput()} value={inputHidden}/>
+            {inputHidden ? "" : <span >
+                Show text
+                <input type="checkbox" onClick={() => toggleBoxes()}/>
+                Text size
+                <input type="number" maxLength="3" defaultValue="100"/>
+                Columns
+                <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                </select>
+                </span>
+            }
+        </p>
+    );
+
 
 Configurations.propTypes = {
-    greet: React.PropTypes.string,
-    message: React.PropTypes.string
+    inputHidden: PropTypes.bool.isRequired,
+    toggleShowInput: PropTypes.func.isRequired,
+    toggleBoxes: PropTypes.func.isRequired
 };
 
 export default Configurations;
+
+
+
--- app/components/SilhouetteApp.jsx
+++ app/components/SilhouetteApp.jsx
@@ -1,8 +1,12 @@
 import React, {PropTypes}from 'react';
 import Configurations  from './Configurations.jsx';
 import Silhouettes  from './Silhouettes.jsx';
+import { createStore } from 'redux';
+
+
 
 export default class SilhouetteApp extends React.Component {
+
 
     defaultProps = {
         sentence : "Default sentence"
@@ -11,7 +15,9 @@ export default class SilhouetteApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            sentence: "This is a text,\nfor your pleasure,\nfor my fun,\nwin win ",
+            sentence: "This is a text ,\nfor your pleasure,\nfor my fun,\nwin win ",
+            inputHidden: false,
+            boxes: true,
         };
         this.sentenceChanged= this.sentenceChanged.bind(this);
 
@@ -26,10 +32,16 @@ export default class SilhouetteApp extends React.Component {
 
             <div>
                 <section>
-                    <Configurations />
+                    <Configurations
+                        inputHidden={this.state.inputHidden}
+                        toggleShowInput={() => this.setState({inputHidden:!this.state.inputHidden})}
+                        toggleBoxes={() => this.setState({boxes:!this.state.boxes})}
+                    />
                 </section>
                 <section>
-                    <textarea rows="10" cols="25" onChange={this.sentenceChanged}>{this.state.sentence}</textarea>
+                    {this.state.inputHidden ? "" :
+                        <textarea rows="10" cols="25" onChange={this.sentenceChanged}>{this.state.sentence}</textarea>
+                    }
                     <Silhouettes sentence={this.state.sentence}/>
                 </section>
             </div>
