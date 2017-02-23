0a4daa8e9236ff1b6b18bb1794d4f3cf14f9b604
Nils Larsgård <nilsmagnus@gmail.com>
2017-02-23 19:35:29 +0100

    Fill 1 line with name

--- app/components/Rune.jsx
+++ app/components/Rune.jsx
@@ -17,26 +17,26 @@ export default class Rune extends React.Component {
             return (<span className="symbol" style={scaleStyle}> </span>);
         }
         if (/^[A-Z\u00D8\u00C6\u00C5]+$/.test(character)) {
-            height = 30;
-            width = 30;
+            height = 42;
+            width = 42;
         }
         else if (/^[pgjy]+$/.test(character)) {
-            height = 30;
-            width = 20;
-            scaleStyle.bottom = -10;
+            height = 42;
+            width = 28;
+            scaleStyle.bottom = -14;
         }
         else if (/^[0-9khtlbd]+$/.test(character)) {
-            height = 30;
-            width = 20;
+            height = 42;
+            width = 28;
         }
         else if (/^[f]+$/.test(character)) {
-            height = 40;
-            width = 20;
-            scaleStyle.bottom=-10;
+            height = 56;
+            width = 28;
+            scaleStyle.bottom=-14;
         }
         else if (/^[a-z\u00E5\u00F8\u00E6]+$/.test(character)) {
-            height = 20;
-            width = 20;
+            height = 28;
+            width = 28;
         }
         scaleStyle.minHeight = (height * (1 + scale / 100))
         scaleStyle.minWidth = (width * (1 + scale / 100));
