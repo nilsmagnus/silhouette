260871ccedc166a5acfc6940088e10607064fdac
Nils Larsgård <nilsmagnus@users.noreply.github.com>
2017-03-21 14:30:28 +0100

    Update npm-bootstrap.sh

--- npm-bootstrap.sh
+++ npm-bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm install webpack webpack-dev-server babel-loader babel-preset-es2015 babel-preset-react babel-core --save-dev
 npm install react react-dom --save
-echo '{ presets" : ["es2015", "react"] }' > .babelrc
+echo '{ "presets" : ["es2015", "react"] }' > .babelrc
